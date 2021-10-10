6b22b57361e1bb3b92b7ff695778694d2ca89e28
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2021-10-10 13:04:31 -0700

    Updated icons

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -19,10 +19,49 @@ module.exports = {
   head: [
     ["meta", { name: "theme-color", content: "#3eaf7c" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    ["meta", { name: "msapplication-TileColor", content: "#da532c" }],
+    ["meta", { name: "theme-color", content: "#ffffff" }],
     [
       "meta",
       { name: "apple-mobile-web-app-status-bar-style", content: "black" },
     ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "public/apple-touch-icon.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "public/favicon-16x16.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "public/favicon-32x32.png",
+      },
+    ],
+
+    ["link", { rel: "manifest", href: "public/site.webmanifest" }],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        color: "#5bbad5",
+        href: "public/safari-pinned-tab.svg",
+      },
+    ],
+    ["link", { rel: "icon", href: "public/favicon.ico" }],
   ],
 
   /**
@@ -42,8 +81,8 @@ module.exports = {
         link: "/guide/",
       },
       {
-        text: "Config",
-        link: "/config/",
+        text: "Examples",
+        link: "/examples/",
       },
       {
         text: "Github",
