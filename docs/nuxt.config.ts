60052dbb2e52810b7027ef467b14b488d08fb656
Sanjay Soundarajan <sanjay.16@live.com>
2023-06-23 15:06:01 -0700

    docs: add umami

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,6 +2,18 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
 
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://umami.sanjaysoundarajan.dev/mushroom',
+          async: true,
+          'data-website-id': '076e376b-e7ba-457e-85e3-c3e7fc55ec61',
+        },
+      ],
+    },
+  },
+
   modules: [
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible',
